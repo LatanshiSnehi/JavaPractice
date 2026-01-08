a276b7fa0c0acf8271479b578e7fd94a934722e5
Latanshi Snehi <latanshisnehi@gmail.com>
2026-01-08 15:02:50 +0530

    Day2 practice

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome{
+    public static boolean palindrome(String str){
+        int i = 0;
+        int j = str.length()-1;
+        while(i<j){
+          
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }             
+        return true;
+    }
+
+    public static void main(String[] args){
+        String str = "racecar";
+        System.out.println(palindrome(str));
+    }
+}
