d37823eac1083effb8314730a62a65ccb254b513
Latanshi Snehi <latanshisnehi@gmail.com>
2026-01-16 11:34:20 +0530

    Day9

---
+++ Day9.java
@@ -0,0 +1,35 @@
+public class Day9 {
+    public static void day9(String str){
+        int n = str.length();
+        char[] c = str.toCharArray();
+        for(int i = 0; i<n ; i++){
+            boolean seen = false;
+            int count =0;
+            
+            for(int j =0; j<i; j++){
+                if(c[i]==c[j]){
+                    seen = true;
+                    break;
+                }
+              
+            }
+            if(seen){
+                continue;
+            }
+
+            for(int k=0; k<n;k++){
+                if(c[i]==c[k]){
+                    count++;
+                }
+                    
+                }
+            
+            System.out.println(c[i] +"=>"+ count);
+        }
+        
+    }
+
+    public static void main(String[] args){
+        day9("banana");
+    }
+}
