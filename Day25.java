d40b88d4cc948d1a28cdc94fdadb2acd1c19d332
Latanshi Snehi <latanshisnehi@gmail.com>
2026-02-17 14:44:33 +0530

    Day25

---
+++ Day25.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class Day25 {
+    public static boolean findDuplicate(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < nums.length;i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3};
+        System.out.println(findDuplicate(arr));
+    }
+}
